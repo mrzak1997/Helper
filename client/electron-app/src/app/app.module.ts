7963ecadfdf01f5a2cd81888d410727bee2a10dd
javad sheikh <deroom24@gmail.com>
2022-02-21 21:11:32 +0330

    day 1 of coding - header + login

--- client/electron-app/src/app/app.module.ts
+++ client/electron-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TitleBarComponent } from './Components/shared/title-bar/title-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleBarComponent
   ],
   imports: [
     BrowserModule,
