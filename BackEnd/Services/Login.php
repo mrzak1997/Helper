b3a5a9deafeeec1ef0351179dbbf9f98bead6c3c
redstarnight <redstarnight1@gmail.com>
2022-02-27 22:12:04 +0330

    fixed Response bug

--- BackEnd/Services/Login.php
+++ BackEnd/Services/Login.php
@@ -1,13 +1,17 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
+
     include "..\config.php";
     include "..\Controller\DefaultSettings.php";
     include "..\Controller\Authentication.php";
     
     $Authentication = new Authentication();
+    
+    $json = file_get_contents('php://input');
+    $data = json_decode($json);
 
-    if(isset($_POST["username"]) && isset($_POST["password"])){
+    if((isset($data->username) && isset($data->password)) || (isset($_POST["username"]) && isset($_POST["password"]))){
         
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             $ip = $_SERVER['HTTP_CLIENT_IP'];
@@ -18,8 +22,8 @@
         }
         
         $user["ip"]=$ip;
-        $user["username"] = $_POST["username"];
-        $user["password"] = $_POST["password"];
+        $user["username"] = isset($data->username) ?? isset($_POST["username"]);
+        $user["password"] = isset($data->password) ?? isset($_POST["password"]);
 
         echo $Authentication->main($user);
     }else{
