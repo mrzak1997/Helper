67585044e09035a5498efa80da12d890867b402d
mrzak1997 <95252566+mrzak1997@users.noreply.github.com>
2022-02-28 12:10:20 +0330

    Update user_Information.php

--- BackEnd/Services/user_Information.php
+++ BackEnd/Services/user_Information.php
@@ -7,14 +7,17 @@
     
     $Authentication = new Authentication();
     
-    $json = file_get_contents('php://input');
-    $data = json_decode($json);
+   // $json = file_get_contents('php://input');
+   // $data = json_decode($json);
 
-    if((isset($data->username) && isset($data->Token)) || 
-        (isset($_POST["username"]) && isset($_POST["Token"]))){
-
-        $information["username"] = isset($data->username) != null  ? $data->username : $_POST["username"];
-        $information["Token"] = isset($data->Token) != null ? $data->Token : $_POST["Token"];
+    //if((isset($data->username) && isset($data->Token)) || 
+     //   (isset($_GET["username"]) && isset($_GET["Token"]))){
+    if(isset($_GET["username"]) && isset($_GET["Token"])){
+        $information["username"] = isset($_GET["username"]) != null  ? $_GET["username"];
+        $information["Token"] = isset($_GET["Token"]) != null ? $_GET["Token"];
+        
+        //$information["username"] = isset($data->username) != null  ? $data->username : $_POST["username"];
+        //$information["Token"] = isset($data->Token) != null ? $data->Token : $_POST["Token"];
         echo $Authentication->getUserinformation($information);
     }else{
         echo "not found";
