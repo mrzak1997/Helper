0b752a1978f58c08ecf95d0051b187352486b8a8
redstarnight <redstarnight1@gmail.com>
2022-02-28 20:24:31 +0330

    updated user_information.php

--- BackEnd/Services/user_Information.php
+++ BackEnd/Services/user_Information.php
@@ -7,17 +7,11 @@
     
     $Authentication = new Authentication();
     
-   // $json = file_get_contents('php://input');
-   // $data = json_decode($json);
-
-    //if((isset($data->username) && isset($data->Token)) || 
-     //   (isset($_GET["username"]) && isset($_GET["Token"]))){
     if(isset($_GET["username"]) && isset($_GET["Token"])){
+        
         $information["username"] = isset($_GET["username"]) != null  ? $_GET["username"]:null;
         $information["Token"] = isset($_GET["Token"]) != null ? $_GET["Token"]:null;
         
-        //$information["username"] = isset($data->username) != null  ? $data->username : $_POST["username"];
-        //$information["Token"] = isset($data->Token) != null ? $data->Token : $_POST["Token"];
         echo $Authentication->getUserinformation($information);
     }else{
         echo "not found";
