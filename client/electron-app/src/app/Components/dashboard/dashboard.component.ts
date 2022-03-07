3f5b27f4428548d54295d08fdafcd105731a27ee
javad sheikh <deroom24@gmail.com>
2022-03-07 19:57:47 +0330

    dashboard v1

--- client/electron-app/src/app/Components/dashboard/dashboard.component.ts
+++ client/electron-app/src/app/Components/dashboard/dashboard.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  menu_item = ['منو','منو','منو','منو','منو ',]
   constructor() { }
 
   ngOnInit(): void {
--- client/electron-app/src/app/Components/dashboard/dashboard.module.ts
+++ client/electron-app/src/app/Components/dashboard/dashboard.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
-
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -9,7 +10,9 @@ import { DashboardComponent } from './dashboard.component';
     DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatGridListModule,
+    MatIconModule
   ]
 })
 export class DashboardModule { }
