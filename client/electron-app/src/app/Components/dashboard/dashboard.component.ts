cfefb7b455983ea1305f651e34bfed3c9af1925e
javad sheikh <deroom24@gmail.com>
2022-03-08 01:18:10 +0330

    dashboard v2 - menu

--- client/electron-app/src/app/Components/dashboard/dashboard.component.ts
+++ client/electron-app/src/app/Components/dashboard/dashboard.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  menu_item = ['منو','منو','منو','منو','منو ',]
+  menu_item = ['منو','منو','منو','منو','منو '];
+  menu_closed = true;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  close_menu(){
+    this.menu_closed = !this.menu_closed
+    
+  }
 }
