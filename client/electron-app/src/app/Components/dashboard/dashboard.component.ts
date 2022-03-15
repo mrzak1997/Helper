2eb5bba6ed8910e7759e9bb9c824dab2db6ac7af
javad sheikh <deroom24@gmail.com>
2022-03-15 14:53:39 +0330

    minor

--- client/electron-app/src/app/Components/dashboard/dashboard.component.ts
+++ client/electron-app/src/app/Components/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@
 import { SystemService } from './../../services/api/system.service';
 
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,22 +10,29 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  currentComponent = "0";
   height = '50px';
   menu_items:any ;
   overflow = 'hidden';
   time:any;
   menu_item = ['منو','منو','منو','منو','منو '];
   menu_closed = true;
-  constructor(private eleRef: ElementRef, private sysService: SystemService,private route: ActivatedRoute,private renderer: Renderer2) {
-    console.log(this.route.snapshot.children[0]?.routeConfig?.path);
+  constructor(private eleRef: ElementRef, private sysService: SystemService,private route: ActivatedRoute,
+    private renderer: Renderer2, private router: Router) {
+      
+      
     this.sysService.getMenuItems().subscribe(res=>{
       this.menu_items = res.body
       console.log(this.menu_items)
+      this.currentComponent = this.router.url.substring(this.router.url.lastIndexOf('/') + 1);
+      this.current_active_menu(this.currentComponent)
     })
    }
 
   ngOnInit(): void {
+   
   }
+  
   close_menu(){
     this.overflow= 'hidden';
     this.menu_closed = !this.menu_closed
@@ -35,38 +42,55 @@ export class DashboardComponent implements OnInit {
       }, 500);
     }
   } 
-  menu_click(target:any,count:any){
-    let opened = this.eleRef.nativeElement.querySelector('.opened');
-    if(opened != null){
-        this.renderer.removeClass(opened,'opened')
-        this.renderer.setStyle(opened,'height','50px')
+  menu_click(target:any,item:any){
+    
+    if(item.sub_items != ""){
+      let classes = target.getAttribute("class");
+      if(classes.indexOf('menu_item_stuff') > -1){// clicked on menu_stuff
+        if(classes.indexOf('down') > -1){
+          this.renderer.removeClass(target,'down')
+        }else{
+          this.renderer.addClass(target,'down')
+        }
+      }else{                                     // clicked on insides of menu_stuff
+        target = this.renderer.parentNode(target)
+        classes = target.getAttribute("class");
+        if(classes.indexOf('down') > -1){
+          this.renderer.removeClass(target,'down')
+        }else{
+          this.renderer.addClass(target,'down')
+        }
+      }
+      let parent = this.renderer.parentNode(target);
+      classes = parent.getAttribute("class");
+        if(classes.indexOf('opened') > -1){
+          this.renderer.removeClass(parent,'opened')
+          this.renderer.setStyle(parent,'height','50px')
+        }else{
+          let opened = this.eleRef.nativeElement.querySelector('.opened');
+          if(opened != null ){
+              this.renderer.removeClass(opened,'opened')
+              this.renderer.setStyle(opened,'height','50px')
+          }
+          this.renderer.addClass(parent,'opened')
+          let height = ((item.sub_items.length*50) +50 ) + 'px'
+          this.renderer.setStyle(parent,'height',height)
+        }
+    }else{
+      console.log(item)
+      if(item.parent_name == "فلوچارت"){
+        if(item.name == "ویرایش و حذف"){
+          console.log('go')
+          this.router.navigate(['dashboard/create-flowchart'])
+        }
+      }
     }
-    let classes = target.getAttribute("class");
-    if(classes.indexOf('menu_item_stuff') > -1){// clicked on menu_stuff
-      
-      if(classes.indexOf('down') > -1){
-        this.renderer.removeClass(target,'down')
-      }else{
-        this.renderer.addClass(target,'down')
-      }
-    }else{                                     // clicked on insides of menu_stuff
-      target = this.renderer.parentNode(target)
-      classes = target.getAttribute("class");
-      if(classes.indexOf('down') > -1){
-        this.renderer.removeClass(target,'down')
-      }else{
-        this.renderer.addClass(target,'down')
-      }
+  }
+  current_active_menu(path:string){
+    switch(path){
+      case 'create-flowchart':
+        console.log("active create flowchart")
+        break;
     }
-    let parent = this.renderer.parentNode(target);
-    classes = parent.getAttribute("class");
-      if(classes.indexOf('opened') > -1){
-        this.renderer.removeClass(parent,'opened')
-        this.renderer.setStyle(parent,'height','50px')
-      }else{
-        this.renderer.addClass(parent,'opened')
-        let height = ((count*50) +50 ) + 'px'
-        this.renderer.setStyle(parent,'height',height)
-      }
   }
 }
